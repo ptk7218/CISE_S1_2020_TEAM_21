6675c27b5488b4106eef2f0bc539a81c6b84f039
shixuan Wang <62128097+ptk7218@users.noreply.github.com>
2020-06-05 10:01:40 +1200

    Create dateprocess.php

---
+++ dateprocess.php
@@ -0,0 +1,61 @@
+<?php
+include_once('link.php');
+include_once('header1.php');
+require_once('connection.php');
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <!-- head -->
+    <head>
+        <meta charset = "UTF-8">
+        <meta name = "viewport" content = "width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content = "ie=edge">
+
+        <!-- Font Awesome kit's code -->
+        <script src="https://kit.fontawesome.com/4e6ccfc44a.js" crossorigin="anonymous"></script> 
+       
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Chelsea+Market&family=Lora&display=swap" rel="stylesheet">
+
+        <!-- Custom Styling -->
+        <link rel="stylesheet" type="text/css" href = "css/style1.css">
+    </head>
+
+       <body>
+       	<h1>result</h1>
+    <div class="article-container">
+<?php
+    
+    	
+    	$txtStartDate=$_GET['txtStartDate'];
+   	    $txtEndDate=$_GET['txtEndDate'];
+
+    	$sql =  "SELECT * FROM article WHERE a_date BETWEEN '$txtStartDate' AND '$txtEndDate' "; 
+
+    	$result = mysqli_query($conn, $sql);
+    	
+    	if (!$result) {
+          
+            echo "<p>Something is Wrong with", $sql, "</p>";            
+            }
+    	else {
+    		while($row = mysqli_fetch_assoc($result)){
+              echo "<div class='article-box'>
+                <h3>".$row['a_method']."</h3>
+                <p>".$row['a_author'].".,& ".$row['a_author2'].". ".$row['a_title'].". ".$row['a_journal']. ", ".
+                $row['a_volume']."(".$row['a_id']."), ".$row['a_pages'].". ".$row['a_link']. "</p>
+             </div>";
+    	}
+     mysqli_free_result($result);
+    }
+  mysqli_close($conn);
+?>
+
+</div>
+
+ <?php include("includes/footer.php"); ?>
+
+</body>
+</html>
