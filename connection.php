eea93a4acdb0a1766e4fe48a0b3c0fe27c6e348f
shixuan Wang <62128097+ptk7218@users.noreply.github.com>
2020-06-05 20:25:50 +1200

    undate

--- connection.php
+++ connection.php
@@ -1,5 +1,6 @@
 <!--
-in this file we write code for connection with database.
+in this file we write code for connection with database and check if
+it is connect with the database
 -->
 <?php
 $conn = mysqli_connect("us-cdbr-east-05.cleardb.net","be11fd0b4d6c79","b8b197d2","heroku_8da37fa6c7415cc");
