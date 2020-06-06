e759492bfbb80968ca690595b23334aaf4011328
shixuan Wang <62128097+ptk7218@users.noreply.github.com>
2020-06-06 17:27:40 +1200

    update info

--- updateInfo.php
+++ updateInfo.php
@@ -2,27 +2,12 @@
                <h1 class="recent-post-title"> </h1>
                  <div class ="post">
                      <div class="post-preview">
-                         <h2><a href ="single.html">Update Information</a> </h2>
+                         <h2><a href ="single.html">Newest Update</a> </h2>
                          <i class="far fa-user"> Administrator: Shixuan Wang</i>
                          &nbsp;
                          <i class = "far calendar">May 20,2020</i>
                          <p class="preview-text">
-                         01/06/2020 update: SEER version 2.0
-                         1. add a different home page after login.
-                         
-                         02/06/2020 update: SEER version 2.1
-                         1. add user roles into SEER.
-                         2. add a different home page after user login.
-                         3. add article database search function to main page.
-                         4. add user profile into SEER
-                         5.create SEER logo 
 
-                         04/06/2020 update: SEER version 2.2
-                         1. add advance search option into welcome website. (after login)
-                         2. add year range search function inside advance search
-                         3. BUG: in user profile there is only Email shows.
-                         4. add SEER logo into header.
-                         
                          06/06/2020 update: SEER version 3.0
                          1. add admin, analytic, moderator roles in SEER
                          2. moderator register/login system still not functional
@@ -30,7 +15,7 @@
                          4. add different management UI (depend on different roles)
                          
                          </p>
-                         <a href = "" class="btn read-more">Read More</a>
+                         <a href = "updatehistory.php" class="btn read-more">Update History</a>
                      </div>
                  </div>             
   
---
+++ updatehistory.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+    <!-- head -->
+    <head>
+        <meta charset = "UTF-8">
+        <meta name = "viewport" content = "width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content = "ie=edge">
+
+        <!-- Font Awesome kit's code -->
+        <script src="https://kit.fontawesome.com/4e6ccfc44a.js" crossorigin="anonymous"></script> 
+       
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Chelsea+Market&family=Lora&display=swap" rel="stylesheet">
+
+        <!-- Custom Styling -->
+        <link rel="stylesheet" type="text/css" href = "css/style1.css">
+
+        <title>SEER Search</title>  
+    </head>
+
+    <body>
+     
+    <?php include("includes/header2.php"); ?>                         
+                         
+                         
+                         <h2> 01/06/2020 update: SEER version 2.0 </h2>
+                         <p>1. add a different home page after login.</p>
+                         
+                         <h2>02/06/2020 update: SEER version 2.1</h2>
+                         <p> 1. add user roles into SEER.<br> 
+                        2. add a different home page after user login.<br>
+                         3. add article database search function to main page.<br>
+                         4. add user profile into SEER<br>
+                         5.create SEER logo </p>
+
+                         <h2>04/06/2020 update: SEER version 2.2</h2>
+                         <p>1. add advance search option into welcome website. (after login)<br>
+                         2. add year range search function inside advance search<br>
+                         3. BUG: in user profile there is only Email shows.<br>
+                         4. add SEER logo into header.</p>
+                         
+                         <h2>06/06/2020 update: SEER version 3.0</h2>
+                         <p>1. add admin, analytic, moderator roles in SEER<br>
+                         2. moderator register/login system still not functional<br>
+                         3. login/register system header and footer style fixed.<br>
+                         4. add different management UI (depend on different roles)</p>
+
+</body>
+<?php include("includes/header2.php"); ?>       
+</html>
